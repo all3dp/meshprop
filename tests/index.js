10a2d02a6d4114caf90d7b41f685d71dabec7d59
Alex <alexander@craftcloud3d.com>
2025-02-26 14:01:32 +0100

    Fix __dirname reference in test

--- tests/index.js
+++ tests/index.js
@@ -1,5 +1,10 @@
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
 import assert from 'assert';
 import meshprop from '../index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 describe("meshprop tests", function () {
     it("should get correct results for the fixture", function (done) {
